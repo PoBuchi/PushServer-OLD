31815240f5c43473e89db91be8160e037b33c677
Pauli B <pauli.buccini@gmail.com>
2015-11-11 15:22:50 +0700

    added linter

--- server/lib/collections.js
+++ server/lib/collections.js
@@ -1,5 +1,3 @@
-Notifications = new Meteor.Collection('notifications')
-Alerts = new Meteor.Collection('alerts')
-Certificates = new Meteor.Collection('certificates')
-// AgendaTimes = new Meteor.Collection('agendatimes')
-// Agenda = new Meteor.Collection('agenda');
+Notifications = new Meteor.Collection('notifications');
+Alerts = new Meteor.Collection('alerts');
+Certificates = new Meteor.Collection('certificates');
